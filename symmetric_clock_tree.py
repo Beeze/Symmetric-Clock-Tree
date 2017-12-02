56bd4b0a9c882e8c112fbf3b2fe3cf6194dd25fa
Marc <m.brown298@gmail.com>
2017-12-02 18:49:44 -0500

    able to group sinks with their centroid. Also added plot attributes to differentiate between centroids and sinks.

--- symmetric_clock_tree.py
+++ symmetric_clock_tree.py
@@ -4,6 +4,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from pprint import pprint
 
+class PlotAttrs:
+    def __init__(self):
+        # matplot markers: https://matplotlib.org/api/markers_api.html
+        self.centroid_attrs = ['rv', 'bv', 'gv', 'kv', 'mv']
+        self.sink_attrs = ['ro', 'bo', 'go', 'ko', 'mo']
 
 class Sink:
     def __init__(self, x, y):
@@ -39,68 +44,6 @@ class SymmetricClockTree:
         self.minYCoordinate = 0
         self.maxYCoordinate = 2500
 
-    def addCentroid(self, centroid, i):
-        self.centroids.append(centroid)
-
-    def createSinkGroup(self, centroid, i):
-        sinkGroupDict = {
-            "centroid_location": [centroid.location[0], centroid.location[1]],
-            "sinks": []
-        }
-
-        self.sinkGroups[i] = sinkGroupDict
-
-    def addSinksToGroups(self, group_ids):
-        for i, group_id in enumerate(group_ids):
-            self.sinkGroups[group_id]["sinks"].append(self.sinks[i])
-
-        pprint(self.sinkGroups)
-
-    def groupSinks(self):
-        sinks = self.getSinkLocations()
-        kmeans = KMeans(n_clusters=4).fit(sinks)
-        centroids = kmeans.cluster_centers_
-
-        for i, location in enumerate(centroids):
-            centroid = Centroid(location[0], location[1])
-            self.addCentroid(centroid, i)
-            self.createSinkGroup(centroid, i)
-
-        self.addSinksToGroups(kmeans.labels_)
-
-    def findAllFactors(self):
-        n = self.num_sinks
-        return sorted(set(reduce(list.__add__,
-                ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0))))
-
-    def getSinkLocations(self):
-        locations = []
-        for sink in self.sinks:
-            locations.append(sink.location)
-        return locations
-
-    def makePlot(self):
-        self.plotSinks()
-        self.plotCentroids()
-        self.setAxis()
-        self.showPlot()
-
-    def plotSinks(self):
-        for sink in self.sinks:
-            plt.plot(sink.location[0], sink.location[1], 'ro')
-
-    def setAxis(self):
-        plt.axis([self.minXCoordinate, self.maxXCoordinate, self.minYCoordinate, self.maxYCoordinate])
-
-
-    def showPlot(self):
-        plt.show()
-
-    def plotCentroids(self):
-        for centroid in self.centroids:
-            plt.plot(centroid.location[0], centroid.location[1], 'go')
-
-
     def generateRandomSinkLocations(self):
         radius = 200
         rangeX = (self.minXCoordinate, self.maxXCoordinate)
@@ -125,8 +68,71 @@ class SymmetricClockTree:
             i += 1
             excluded.update((x+dx, y+dy) for (dx,dy) in deltas)
 
+    def groupSinks(self):
+        sinks = self.getSinkLocations()
+        kmeans = KMeans(n_clusters=4).fit(sinks)
+        centroids = kmeans.cluster_centers_
+
+        for i, location in enumerate(centroids):
+            centroid = Centroid(location[0], location[1])
+            self.addCentroid(centroid, i)
+            self.createSinkGroup(centroid, i)
+
+        self.addSinksToGroups(kmeans.labels_)
+
+    def getSinkLocations(self):
+        locations = []
+        for sink in self.sinks:
+            locations.append(sink.location)
+        return locations
+
+    def addCentroid(self, centroid, i):
+        self.centroids.append(centroid)
+
+    def createSinkGroup(self, centroid, i):
+        sinkGroupDict = {
+            "centroid_location": [centroid.location[0], centroid.location[1]],
+            "sinks": []
+        }
+
+        self.sinkGroups[i] = sinkGroupDict
+
+    def addSinksToGroups(self, group_ids):
+        for i, group_id in enumerate(group_ids):
+            self.sinkGroups[group_id]["sinks"].append(self.sinks[i])
+
+    def findAllFactors(self):
+        n = self.num_sinks
+        return sorted(set(reduce(list.__add__,
+                ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0))))
+
+    def makePlot(self):
+        self.setAxis()
+        self.plotSinkGroups()
+        self.showPlot()
+
+    def setAxis(self):
+        plt.axis([self.minXCoordinate, self.maxXCoordinate, self.minYCoordinate, self.maxYCoordinate])
+
+    def plotSinkGroups(self):
+        plotAttrs = PlotAttrs()
+        centroid_attrs = plotAttrs.centroid_attrs
+        sink_attrs = plotAttrs.sink_attrs
+
+        for group_id in self.sinkGroups:
+            # plot centroid
+            centroid_location = self.sinkGroups[group_id]["centroid_location"]
+
+            plt.plot(centroid_location[0], centroid_location[1], centroid_attrs[group_id])
+            # plot sinks
+            for sink in self.sinkGroups[group_id]["sinks"]:
+                plt.plot(sink.location[0], sink.location[1], sink_attrs[group_id])
+
+    def showPlot(self):
+        plt.show()
+
 
 tree = SymmetricClockTree(10, 1)
 tree.generateRandomSinkLocations()
 tree.groupSinks()
-#tree.makePlot()
+tree.makePlot()
