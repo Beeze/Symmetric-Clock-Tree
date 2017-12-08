b74ee71deec89dd8af378413325d1112ad078535
Marc <m.brown298@gmail.com>
2017-12-07 23:32:01 -0500

    code cleaned up, next step is dynamically choosing kmeans groupings via silhouette analysis.

--- symmetric_clock_tree.py
+++ symmetric_clock_tree.py
@@ -29,7 +29,7 @@ class Centroid:
     # Since we're only operating with one level for now, this isn't used.
     def set_number_of_sinks(self, n):
         self.num_sinks = n
-
+        
 # Represents the traits of our plot.
 # Each element in the arrays correspond to a SinkGroup.
 class PlotAttrs:
@@ -56,9 +56,24 @@ class SymmetricClockTree:
         self.standardizedWireLength = 0
         self.directionTraveled = { "up": False, "down": False, "left": False, "right": False }
 
+    # Helper method to calculate the distance between two points.
+    def DistanceToGoal(self, start, goal):
+        return math.sqrt(
+            ((goal[0] - start[0]) ** 2) +
+            ((goal[1] - start[1]) ** 2)
+        )
+
+    # reset's our directionTraveled Dictionary
+    def resetDirectionTravel(self):
+        self.directionTraveled = { "up": False, "down": False, "left": False, "right": False }
+
+    # getter method for get
+    def getDirectionTraveled(self):
+        return self.directionTraveled
+
     # Helper method for generating random sink locations.
     def generateRandomSinkLocations(self):
-        radius = 500
+        radius = 1000
         rangeX = (self.maxXCoordinate/4, (3*self.maxXCoordinate/4))
         rangeY = (self.maxYCoordinate/4, (3*self.maxYCoordinate/4))
         qty = self.num_sinks  # or however many points you want
@@ -68,7 +83,7 @@ class SymmetricClockTree:
         deltas = set()
         for x in range(-radius, radius+1):
             for y in range(-radius, radius+1):
-                if x*x + y*y <= radius*radius and x*x <= self.maxXCoordinate and y*y <= self.maxYCoordinate:
+                if x*x + y*y <= radius*radius and x*x <= (self.maxXCoordinate*2) and y*y <= (self.maxYCoordinate):
                     deltas.add((x,y))
 
         excluded = set()
@@ -161,7 +176,7 @@ class SymmetricClockTree:
     # Sets the axes for our plot.
     def setAxis(self):
         # ([minX, maxX, minY, maxY])
-        plt.axis([self.minXCoordinate, 3*self.maxXCoordinate/2, self.minYCoordinate, 3*self.maxYCoordinate/2])
+        plt.axis([self.minXCoordinate, (self.maxXCoordinate + self.maxXCoordinate/5), self.minYCoordinate, (self.maxYCoordinate + self.maxXCoordinate/5) ])
 
     # Shows our plot
     def showPlot(self):
@@ -227,62 +242,80 @@ class SymmetricClockTree:
 
         direction_traveled = { "up": False, "down": False, "left": False, "right": False }
 
+        # turning tracks which axis we're traveling on.
         turning = False
-        go = True
-        firstLine = True
-        finalLine = False
 
+        continue_drawing_line = True
+
+        # booleans that track which point we're going to or coming from.
+        connect_to_start = True
+        connect_to_goal = False
+
+        # Initialize our wire length
         current_wire_length = 0
 
-        while go:
+        while continue_drawing_line:
             # get relative direction
-            direction = self.getSinkRelativeCentroidDirection(start, goal, turning)
+            direction = self.getCurrentLocationRelativeToCentroid(start, goal, turning)
+
+            # initialize recalculated_direction
             recalculated_direction = direction
 
+            # determine the best direction to begin drawing
             direction_to_travel = self.getNextDirectionToTravel(start, goal, turning)
 
+            # movingInPositiveDirection tracks which axis we're moving in
             movingInPositiveDirection = False if (direction_to_travel == "left" or direction_to_travel == "down") else True
+            # movingHorizontally tracks which direction we're moving in
             movingHorizontally = False if (direction_to_travel == "up" or direction_to_travel == "down") else True
 
+            # index tracks which pen position we're changing (x or y)
             index = 0 if movingHorizontally else 1
+
+            # change sets the amount we increment / decrement each time.
             change = 5 if movingInPositiveDirection else -5
 
+            # overshoot_counter tracks how many times we've crossed from the pos -> neg axis, relative to the goal location.
             overshoot_counter = 0
 
-            # calculate the length of the wire
+            # we allow ourselves to cross the axis 5 times
             while overshoot_counter < 5:
-                # ensure we won't hit the graph bounds
+                # check if we've hit the graph bounds.
+                # if we have, we'll trip a boolean so we don't keep trying the same direction
                 if movingInPositiveDirection and pen[index] + change > bounding_thresholds[direction_to_travel]:
                     self.directionTraveled[direction] = True
                     break
                 elif not movingInPositiveDirection and pen[index] + change < bounding_thresholds[direction_to_travel]:
                     self.directionTraveled[direction] = True
                     break
+
                 # check to see if we've passed the goal on this axis.
                 elif recalculated_direction != direction:
                     overshoot_counter += 1
+
+                # update the pen location and current_wire_length
                 pen[index] += change
                 current_wire_length += abs(change)
 
+                # check if we've reached our goal wire length
                 if current_wire_length >= goal_wire_length:
-                    finalLine = True
+                    connect_to_goal = True
                     break
 
-                recalculated_direction = self.getSinkRelativeCentroidDirection(pen, goal, turning)
+                # check our pen's location relative to the centroid
+                recalculated_direction = self.getCurrentLocationRelativeToCentroid(pen, goal, turning)
 
-            if firstLine:
-                print "start to pen"
+            # Check what type of point we're connecting
+            if connect_to_start:
                 self.drawLineBetweenTwoPoints(start, pen, group)
                 last_pen_position = pen[:]
-                firstLine = False
-            elif finalLine:
-                print "final line pen"
+                connect_to_start = False
+
+            elif connect_to_goal:
                 self.drawLineBetweenTwoPoints(pen, goal, group)
-                go = False
+                continue_drawing_line = False
+
             else:
-                print direction
-                print self.directionTraveled
-                print "pen to pen"
                 self.drawLineBetweenTwoPoints(last_pen_position, pen, group)
                 last_pen_position = pen[:]
 
@@ -290,7 +323,7 @@ class SymmetricClockTree:
             turning = not turning
             self.resetDirectionTravel()
 
-
+    # Helper method to draw a line between two points
     def drawLineBetweenTwoPoints(self, start, goal, group):
         plotAttrs = PlotAttrs()
         line_colors = plotAttrs.line_colors
@@ -301,17 +334,25 @@ class SymmetricClockTree:
             color=line_colors[group]
         )
 
-    def getSinkRelativeCentroidDirection(self, start, goal, turning=False):
+    # Helper method which calculates current location relative to centroid
+    def getCurrentLocationRelativeToCentroid(self, start, goal, turning=False):
         if turning:
             return "left" if start[0] > goal[0] else "right"
         else:
             return "down" if start[1] > goal[1] else "up"
 
+    # Helper method to determine which direction we'll travel in
     def getNextDirectionToTravel(self, start, goal, turning=False):
+
+        # Algorithm checks to see which direction we have the most space to travel in.
+        # Once one is selected, we do a lookup to see if we've already tried to go in
+        # that direction during this iteration.
+        # If we have, we try the opposite direction.
+        # If we've tried both, we'll call the function and move in a different
+        # orientation.
         if turning:
             possible_dir = "right" if start[0] > goal[0] else "left"
             if self.directionTraveled[possible_dir] == False:
-                print "here"
                 return possible_dir
             else:
                 new_dir = "left" if possible_dir == "right" else "right"
@@ -335,17 +376,6 @@ class SymmetricClockTree:
                 else:
                     self.resetDirectionTravel()
                     return getNextDirectionToTravel(start, goal, True)
-
-    def DistanceToGoal(self, start, goal):
-        return math.sqrt(
-            ((goal[0] - start[0]) ** 2) +
-            ((goal[1] - start[1]) ** 2)
-        )
-    def resetDirectionTravel(self):
-        print "reset"
-
-    def getDirectionTraveled(self):
-        return self.directionTraveled
 
 
 tree = SymmetricClockTree(50)
